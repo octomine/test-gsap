d90c60046560ba0f07aed7e3563f9fa9b4b521fb
octo <jamine61@mail.ru>
2025-01-26 00:25:29 +0300

    add css inject

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    cssCodeSplit: true,
+  },
+  plugins: [cssInjectedByJsPlugin({ relativeCSSInjection: true })],
 });
