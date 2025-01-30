872b88fb4ab9e7a27cad99bd0a44c7349d407842
octo <jamine61@mail.ru>
2025-01-30 14:51:30 +0300

    resize the bigining

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
 import "./fonts.css";
 import "./style.css";
 
+import { resizables } from "./resizables";
+
 import { showIntro } from "./intro";
 import { startGame } from "./game";
 import { showFinal } from "./final/final";
 
-document.addEventListener('DOMContentLoaded', () => {
+const onResize = () => {
+  const content = document.getElementById('content');
+  const ratio = content.offsetHeight / 810;
+
+  for (let cn in resizables) {
+    const elements = document.getElementsByClassName(cn);
+    const values = resizables[cn];
+    for (let element of elements) {
+      for(let prop in values)
+      element.style[prop] = `${values[prop] * ratio}px`;
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
   document.body.style.visibility = "visible";
+
+  window.onresize = onResize;
+  onResize();
 
   showIntro()
     .then(() => startGame())
---
+++ src/resizables.js
@@ -0,0 +1,165 @@
+export const resizables = {
+  xs: {
+    fontSize: 40,
+  },
+  s: {
+    fontSize: 44,
+  },
+  m: {
+    fontSize: 51,
+  },
+  l: {
+    fontSize: 55,
+  },
+  xl: {
+    fontSize: 59,
+  },
+  xxl: {
+    fontSize: 73,
+  },
+  xxxl: {
+    fontSize: 100,
+  },
+  button: {
+    marginTop: 48,
+    width: 535,
+    height: 150,
+  },
+  bacteria_1: {
+    width: 272,
+    height: 272,
+  },
+  bacteria_2: {
+    width: 209,
+    height: 209,
+  },
+  bacteria_3: {
+    width: 214,
+    height: 214,
+  },
+  bacteria_4: {
+    width: 247,
+    height: 192,
+  },
+  bacteria_5: {
+    width: 264,
+    height: 262,
+  },
+  bacteria_6: {
+    width: 192,
+    height: 210,
+  },
+  bacteria_7: {
+    width: 285,
+    height: 285,
+  },
+  "intro__animation-holder": {
+    width: 980,
+  },
+  "intro__bacteria-holder": {
+    width: 1080,
+    height: 621,
+  },
+  intro__bacteria_1: {
+    top: 430,
+  },
+  intro__bacteria_2: {
+    top: 270,
+  },
+  intro__bacteria_3: {
+    top: 180,
+  },
+  "game-container__bg": {
+    width: 2504,
+  },
+  "dirt-1-1": {
+    left: 31,
+    top: 261,
+    width: 145,
+    height: 258,
+  },
+  "dirt-1-2": {
+    left: 225,
+    top: 102,
+    width: 170,
+    height: 176,
+  },
+  "dirt-2-1": {
+    left: 434,
+    top: 417,
+    width: 148,
+    height: 77,
+  },
+  "dirt-2-2": {
+    left: 612,
+    top: 302,
+    width: 166,
+    height: 72,
+  },
+  "dirt-3-1": {
+    left: 461,
+    top: 127,
+    width: 427,
+    height: 167,
+  },
+  "dirt-4-1": {
+    left: 888,
+    top: 52,
+    width: 277,
+    height: 236,
+  },
+  "dirt-4-2": {
+    left: 778,
+    top: 302,
+    width: 387,
+    height: 61,
+  },
+  "dirt-4-3": {
+    left: 1128,
+    top: 431,
+    width: 37,
+    height: 147,
+  },
+  "dirt-4-4": {
+    left: 826,
+    top: 363,
+    width: 50,
+    height: 140,
+  },
+  "dirt-5-1": {
+    left: 1281,
+    top: 225,
+    width: 96,
+    height: 92,
+  },
+  "dirt-5-2": {
+    left: 1361,
+    top: 324,
+    width: 81,
+    height: 118,
+  },
+  "dirt-5-3": {
+    left: 1400,
+    top: 493,
+    width: 42,
+    height: 75,
+  },
+  "dirt-6-1": {
+    left: 1488,
+    top: 10,
+    width: 199,
+    height: 228,
+  },
+  "dirt-6-2": {
+    left: 1570,
+    top: 273,
+    width: 61,
+    height: 82,
+  },
+  "dirt-7-1": {
+    left: 1872,
+    top: 105,
+    width: 91,
+    height: 433,
+  },
+};
